c48a86e8e966a6864243b72ffedb693a9468f95e
kristijan stiplosek <46843869+kristijan-stiplosek@users.noreply.github.com>
2019-09-23 13:49:33 +0200

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	float temp;
+	
+	std::cout<<"upisi temperaturu ";
+	std::cin>>temp;
+	
+	if (temp>24.5)
+	{
+		std::cout<<"toplo je ";
+	}
+	else
+	{
+		std::cout<<"hladno je ";
+	}
+	
+	
+
+}
